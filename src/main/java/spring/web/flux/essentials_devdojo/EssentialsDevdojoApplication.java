ebc8086d723f341bd44bf3bc8cd018346748ab14
jeanverso <jeanmarcel8@gmail.com>
2024-09-19 22:53:54 -0300

    feat: init project

---
+++ src/main/java/spring/web/flux/essentials_devdojo/EssentialsDevdojoApplication.java
@@ -0,0 +1,13 @@
+package spring.web.flux.essentials_devdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialsDevdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssentialsDevdojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/web/flux/essentials_devdojo/EssentialsDevdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.web.flux.essentials_devdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentialsDevdojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
