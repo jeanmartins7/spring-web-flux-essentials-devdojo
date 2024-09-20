2cbf6dc0c7a400a3112682b8a3741861ead4fb02
jeanverso <jeanmarcel8@gmail.com>
2024-09-20 01:03:22 -0300

    feat: first endpoint with spring webflux

---
+++ src/main/java/spring/web/flux/essentials_devdojo/domain/Anime.java
@@ -0,0 +1,22 @@
+package spring.web.flux.essentials_devdojo.domain;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@With
+@Table("anime")
+public class Anime {
+
+    @Id
+    private Integer id;
+    @NotNull
+    @NotEmpty(message = "The name of this anime cannot be empty")
+    private String name;
+}
